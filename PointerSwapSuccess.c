8eeaf1d6de5506413c72e7ee2774aeab3e352045
doleebest <90204371+doleebest@users.noreply.github.com>
2022-08-09 19:06:16 +0900

    Add files via upload

--- PointerSwapSuccess.c
+++ PointerSwapSuccess.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-void SwapIntPtr(int* p1, int* p2) 
+void SwapIntPtr(int** dp1, int** dp2)
 {
-	int* temp = p1;
-	p1 = p2;
-	p2 = temp;
+	int* temp = *dp1;
+	*dp1 = *dp2;
+	*dp2 = temp;
 }
 
-int main(void) 
+int main(void)
 {
-	int num1 = 10, num2 = 20;
+	int num1 = 10, num2 = 30;
 	int* ptr1, * ptr2;
 	ptr1 = &num1, ptr2 = &num2;
 	printf("*ptr1, *ptr2 : %d %d \n", *ptr1, *ptr2);
 
-	SwapIntPtr(ptr1, ptr2);
+	SwapIntPtr(&ptr1, &ptr2); // ptr1과 ptr2의 주소 값 전달!
 	printf("*ptr1, *ptr2 : %d %d \n", *ptr1, *ptr2);
 	return 0;
-
 }
